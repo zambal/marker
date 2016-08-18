e1db3e1c6210cfd62f3f9e8e3cff140c24f10ac8
Vincent Siliakus <zambal@gmail.com>
2016-08-18 21:26:33 +0200

    add package info to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,13 +1,24 @@
 defmodule Marker.Mixfile do
   use Mix.Project
 
+  @description """
+  Marker strives to be the most convenient tool for writing html markup in Elixir.
+  It allows writing markup with Elixir syntax, while reaching the performance of precompiled templates.
+  """
+
+  @package [
+    name: :marker,
+    files: ~w(lib test mix.exs README.md LICENSE),
+    maintainers: ["Vincent Siliakus"],
+    licenses: ["Apache 2.0"],
+    links: %{"Github" => "https://github.com/zambal/marker"}
+  ]
+
   def project do
     [app: :marker,
      version: "1.0.0",
-     elixir: "~> 1.2"]
-  end
-
-  def application do
-    [applications: []]
+     elixir: "~> 1.2",
+     description: @description,
+     package: @package]
   end
 end
